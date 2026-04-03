e05fdf4ebbd8860bad6261ff58a904edb7844619
Anuj Shrestha <anuj.shrestha@rivieranexus.com>
2026-04-03 12:49:49 -0500

    Fix icons

--- app/plugins/fontawesome.ts
+++ app/plugins/fontawesome.ts
@@ -10,10 +10,42 @@ import {
   faClock,
   faLocationDot,
   faCircleCheck,
+  faExpand,
+  faDownload,
+  faXmark,
+  faChevronUp,
+  faChevronDown,
+  faChevronLeft,
+  faChevronRight,
+  faCalendarCheck,
+  faClockRotateLeft,
+  faArrowUpRightFromSquare,
+  faCircleExclamation,
+  faSpinner,
 } from '@fortawesome/free-solid-svg-icons'
 import {faFacebook} from '@fortawesome/free-brands-svg-icons'
 
-library.add(faGlobe, faEnvelope, faFacebook, faCalendarDays, faClock, faLocationDot, faCircleCheck)
+library.add(
+  faGlobe,
+  faEnvelope,
+  faFacebook,
+  faCalendarDays,
+  faClock,
+  faLocationDot,
+  faCircleCheck,
+  faExpand,
+  faDownload,
+  faXmark,
+  faChevronUp,
+  faChevronDown,
+  faChevronLeft,
+  faChevronRight,
+  faCalendarCheck,
+  faClockRotateLeft,
+  faArrowUpRightFromSquare,
+  faCircleExclamation,
+  faSpinner,
+)
 
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp.component('FontAwesomeIcon', FontAwesomeIcon)
